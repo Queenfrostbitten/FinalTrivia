21b5caf86c6aee333d6c9b70a249fc733a285859
Queen Frost <frostedinflictions24@live.com>
2022-10-14 15:15:58 -0500

    Updates
    updates for final

---
+++ Final/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System{ }
+public string player
+     public static void Main(String[] args)
+    var aGame = new Game(Console.Out);
+    int a, b
+
+     aGame.Add("player1");
+    aGame.Add("player2");
+    aGame.Add("player3");
+
+var random = new Random();
+
+Run(aGame, random);
+Console.ReadLine();
+
+Console.WriteLine("Welcome to Triviabate");
+
+Console.WriteLine("How old are you?");
+if (player <= 17)
+    Console.WriteLine("You are not authorized to play this game.You must be 18 or older.");
+if else player =>18
+            Console.WriteLine("You're 18 or older let's continue!");
+    else(player null)
+    Console.WriteLine("No response was recieved please start over.");
+Console.WriteLine("Choose a username! What would you like your username to be?")
+     
+{
+    
+   
+}
+
+
