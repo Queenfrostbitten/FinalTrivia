cfeb9ec67f98f1e2bf2a24244c59f6c86d7d2aea
Queen Frost <frostedinflictions24@live.com>
2022-10-16 00:11:37 -0500

    Update 10.15.22
    updates added.

--- Final/Program.cs
+++ Final/Program.cs
@@ -4,7 +4,8 @@ public class game
 { public string player;
     public static void Main(String[] args);
     var aGame = new Game(Console.Out);
-    int a, b,
+    List<int> gamequestionNumbers = new List<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+   
 
      aGame.Add("player1");
     aGame.Add("player2");
@@ -13,7 +14,6 @@ public class game
 var random = new Random();
 
     Run(aGame, random);
-    Console.ReadLine();
 
 Console.WriteLine("Welcome to Triviabate");
 
@@ -21,9 +21,10 @@ Console.WriteLine("How old are you?");
 if (player <= 17)
     Console.WriteLine("You are not authorized to play this game.You must be 18 or older.");
 else if player => 18
-            Console.WriteLine("You're 18 or older let's continue!");
+            Console.WriteLine("You're 18 or older let's get the party started!");
     else(player null)
     Console.WriteLine("No response was recieved please start over.");
+
 Console.WriteLine("Choose a username! What would you like your username to be?");
      
 {
