defa9decb14adac111c691d7d9e1310df3c1792c
Martisha <frostedinflictions24@live.com>
2022-11-17 17:38:59 -0500

    while loop
    updates

--- Final/Program.cs
+++ Final/Program.cs
@@ -44,120 +44,124 @@ public class Deck
 
     }
     public class Program
+{
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
 
-        while (Deck.Cards.Count>0)
+        while (Deck.Cards.Count > 0)
         { Console.ReadLine(Choices); }
 
 
-        int[] deck = {c1, c2, c3 c4, c5};
+        int[] deck = { c1, c2, c3 c4, c5 };
         var deck = new Deck();
-            var c1 = new Card
-            {
+        var c1 = new Card
+        {
 
-                ID = 1,
-                Category = " Sexual Health ",
-                Question = "What is the most common STD/STI in the USA?",
-                Answer = "Human papillomavirus (HPV)",
-                Choices = new List<string>()
+            ID = 1,
+            Category = " Sexual Health ",
+            Question = "What is the most common STD/STI in the USA?",
+            Answer = "Human papillomavirus (HPV)",
+            Choices = new List<string>()
                 {
                     "Human papillomavirus (HPV)",
                     "Chlamydia",
                     "Gonorrhea",
                     "Syphilis"
                 }
-            };
-            var c2 = new Card
-            {
-                ID = 2,
-                Category = "Law and Sex",
-                Question = "What is sexual assault?",
-                Answer = "Any type of sexual contact without consent",
-                Choices = new List<string>()
+        };
+        var c2 = new Card
+        {
+            ID = 2,
+            Category = "Law and Sex",
+            Question = "What is sexual assault?",
+            Answer = "Any type of sexual contact without consent",
+            Choices = new List<string>()
                 {
                     "Touching someone on their genitals",
                     "Forcefully grabbing someone by their genitals",
                     "Any type of sexual contact without consent",
                     "hitting someone"
                 }
-            };
+        };
 
-            var c3 = new Card
-            {
-                ID = 3,
-                Category = "Studies and statistics",
-                Question = "What’s the Average Person’s Number of Sexual Partners?",
-                Answer = "7",
-                Choices = new List<string>()
+        var c3 = new Card
+        {
+            ID = 3,
+            Category = "Studies and statistics",
+            Question = "What’s the Average Person’s Number of Sexual Partners?",
+            Answer = "7",
+            Choices = new List<string>()
                 {
                     "2",
                     "15",
                     "44",
                     "7"
                 }
-            };
-            var c4 = new Card
-            {
-                ID = 4,
-                Category = "Studies and statistics",
-                Question = "What percentage of women always reach a climax during sex?",
-                Answer = "29%",
-                Choices = new List<string>()
+        };
+        var c4 = new Card
+        {
+            ID = 4,
+            Category = "Studies and statistics",
+            Question = "What percentage of women always reach a climax during sex?",
+            Answer = "29%",
+            Choices = new List<string>()
                 {
                     "40%",
                     "10%",
                     "65%",
                     "29%"
                 }
-            };
-            var c5 = new Card
-            {
-                ID = 5,
-                Category = "Sexual Health",
-                Question = "Sexually transmitted bacterial infections (STIs) can be treated with what?",
-                Answer = " Antibiotics ",
-                Choices = new List<string>()
+        };
+        var c5 = new Card
+        {
+            ID = 5,
+            Category = "Sexual Health",
+            Question = "Sexually transmitted bacterial infections (STIs) can be treated with what?",
+            Answer = " Antibiotics ",
+            Choices = new List<string>()
                 {
                     "Antibiotics",
                     "Steroids",
                     "Antacids",
                     "antipsychotics"
                 }
-            };
+        };
 
 
 
-            
-            Console.WriteLine("Welcome to Triviabate");
 
-            var _canPlay = GetAgeInputWithMinorCheck();
-            if (!_canPlay) return;
+        Console.WriteLine("Welcome to Triviabate");
 
-            Console.WriteLine("You're 18 or older let's get the party started!");
+        var _canPlay = GetAgeInputWithMinorCheck();
+        if (!_canPlay) return;
+
+        Console.WriteLine("You're 18 or older let's get the party started!");
 
 
-        }
 
-        public static bool GetAgeInputWithMinorCheck()
+    }
+
+    public static bool GetAgeInputWithMinorCheck()
+    {
+        Console.WriteLine("How old are you?");
+        //this way we can handle the parsing result
+        if (int.TryParse(Console.ReadLine(), out var _insertedAge))
         {
-            Console.WriteLine("How old are you?");
-            //this way we can handle the parsing result
-            if (int.TryParse(Console.ReadLine(), out var _insertedAge))
-            {
-                //player is an Adult
-                if (IsAdult(_insertedAge)) return true;
-                //player is a minor
-                Console.WriteLine("You are not authorized to play this game.You must be 18 or older.");
-                return false;
-            }
-            //input is not vaild
-            Console.WriteLine("Invalid Input Exiting Program...");
+            //player is an Adult
+            if (IsAdult(_insertedAge)) return true;
+            //player is a minor
+            Console.WriteLine("You are not authorized to play this game.You must be 18 or older.");
             return false;
         }
-    
-        public static bool IsAdult(int age) => age >= 18;
+        //input is not vaild
+        Console.WriteLine("Invalid Input Exiting Program...");
+        return false;
+    }
+
+
+     
+        {
+    public static bool IsAdult(int age) => age >= 18;
     
         var score = 0;
         foreach (var _card in cards)
@@ -179,25 +183,33 @@ public class Deck
             }
              Console.WriteLine("Wrong Answer");
 
+
+//Main loop
     
-        //Main loop
-       
-           
-         
-         while(Deck.Cards.Count>0);
+
+      var Deck = new Deck();
+
+
+while (Deck.Cards.Count > 0)
 
 {
-         Console.WriteLine("loop here");
-                Deck.Draw() {}
-        
-            Deck.Cards.Add(c1);
-            Deck.Cards.Add(c2);
-            Deck.Cards.Add(c3);
-            Deck.Cards.Add(c4);
-            Deck.Cards.Add(c5);
+    Console.WriteLine("Knowledge Test:{5}");
+
+    Deck.Draw();
+
+    Deck.Cards.Add(c1);
+    Deck.Cards.Add(c2);
+    Deck.Cards.Add(c3);
+    Deck.Cards.Add(c4);
+    Deck.Cards.Add(c5);
 
     Console.WriteLine("Do you wish to continue? Y or N")
         var char = Console.ReadLine();
+
+    if(char == 'N')
+    {
+        wantstoContinue = false;
+            }
 
 
 
